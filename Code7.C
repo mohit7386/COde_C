9f3b980e54dc32647d6f6eccba68094d1d0f04d5
M_O_H_I_T3456 <61095721+mohit7386@users.noreply.github.com>
2024-01-14 13:18:14 +0530

    Changes Updated

--- Code7.C
+++ Code7.C
@@ -4,7 +4,7 @@ int main(){
     int num;
 printf("Enter any Number\n");
 scanf("%d",&num);
-if(num%2==0)
+if(num%2==0) //Here Modulo gives us remainder 0 and in if condition we are checking the condition if it gives the remainder 0 then number is divisible otherwise Not....
 printf("Number is divisible by 2");
 else
 printf("Number is not divisible by 2");
