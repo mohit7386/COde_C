9127b934eb93cea4fd6fd5ed1b5a57f478f76286
M_O_H_I_T3456 <61095721+mohit7386@users.noreply.github.com>
2024-01-14 18:19:52 +0530

    Changes Updated

---
+++ Code9.C
@@ -0,0 +1,45 @@
+//Write the program to check Age wise Category...
+#include<stdio.h>
+int main(){
+    int age;
+    printf("Enter Your Age \n");
+    scanf("%d",&age);
+    if(age>18){
+        printf("You are an Adult\n");
+    }
+    else{
+        printf("You are Not an adult\n");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
