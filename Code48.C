61b82d7b84b168ba20616d36c52f540fe27cc26b
M_O_H_I_T3456 <61095721+mohit7386@users.noreply.github.com>
2024-01-27 15:37:14 +0530

    Changes updated

--- Code48.C
+++ Code48.C
@@ -10,6 +10,6 @@ int main (){
     return 0;
 }
 void calculateSquare(int num){ //Here int num is our parameter and used to receive the value from the function call..
-    float square=pow(num,2); //Pow is a keyword used in to calculate the power of the given number..
+    float square=pow(num,2); //Pow is a keyword used in to calculate the power of the given number..Here we are using float in place of int because 'pow' is a library function and the result of the 'pow' function is a floating point value...so that's why I am using float in place of int for getting the correct answer in result....
     printf("The Square of the %d is: %f\n",num,square); //here I am using float to print the answer in floating point value because the value compile takes in float and if we want the answer in integer then it round off the digits after point and including point also so for getting the full and correct output we need to put the result in float...
 }
--- Code55.C
+++ Code55.C
@@ -1,1 +1,23 @@
-//WAP to print the n terms of the fabonacci - Pronounce this as fabonachi- because it's a italian word 
+//WAP to print the nth term of the fabonacci - Pronounce this as fabonachi- because it's a italian word 
+#include<stdio.h>
+int fib(int n);
+int main(){
+    int n;
+    printf("Enter Which term you want to print in the result\n");
+    scanf("%d",&n);
+    printf("The Fibonacci value of the %dth term is: %d",n,fib(n));
+    return 0;
+}
+//Recursive function 
+int fib(int n){
+    if(n==0){ //Base Case for terminating the recursion
+        return 0;
+    }
+    if(n==1){
+        return 1;
+    }
+    int fibnm1=fib(n-1); //Here function call itself so it's a recirsive function 
+    int fibnm2=fib(n-2);
+    int fibN= fibnm1+fibnm2;
+    return fibN;
+}
