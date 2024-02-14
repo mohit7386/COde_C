76f001e377e766827bb18c1da9ba186c8caaf14e
M_O_H_I_T3456 <61095721+mohit7386@users.noreply.github.com>
2024-02-14 12:37:20 +0530

    New Changes Added

--- Code115.c
+++ Code115.c
@@ -5,14 +5,14 @@ int main(){
     int *ptr;
     ptr=(int *)malloc(5*sizeof(int));//we are typecasting the value of the malloc because gives the pointer in void but we need integer type of memory so that's why we typecasting the value of the malloc and multiplying by the size of the data type..
     //Memory is allocated on the runtime...dynamically allocated memory 
-    ptr[0] = 1;
-    ptr[1] = 3;
+    ptr[0] = 1; //Assigning memory allocation dynamically at 0th location 
+    ptr[1] = 3; //At 1st Position 
     ptr[2] = 5;
     ptr[3] = 7;
     ptr[4] = 9;
     printf("Odd Numbers will Dynamically allocated as: \n");
     for(int i=0;i<5;i++){
-        printf("%d\n",ptr[i]);
+        printf("%d\n",ptr[i]); //Printing all the values which is present at different locations..
     }
 
     return 0;
