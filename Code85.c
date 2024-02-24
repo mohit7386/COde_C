4af32e754498627d5ca8655ef43de493e45bcc49
M_O_H_I_T3456 <61095721+mohit7386@users.noreply.github.com>
2024-02-24 15:12:39 +0530

    New Changes Added

--- Code85.c
+++ Code85.c
@@ -4,12 +4,7 @@ int countLength(char arr[]); //Creating the function for count length of the wor
 int main (){
     char name[50]; //Initialise the array with size= 50 
     fgets(name,50,stdin); //It takes the input from the user-It's a very safe use of fgets pls try to use always fgets in place of gets()...
-     int length = countLength(name);
-    if (length > 0 && name[length - 1] == '\n') {
-        name[length - 1] = '\0';
-    }
-
-    printf("Length of your Entered Letter is: %d", length);
+    printf("Length of your Entered Letter is: %d",countLength(name)); //Print the statement with calling the function countLength 
     return 0;
 }
 int countLength(char arr[]){
@@ -17,6 +12,6 @@ int countLength(char arr[]){
     for (int i=0;arr[i]!='\0';i++){ //Iterating all the values tb tak jab tak ki arr!='\0' nahi jo jaata 
         count++;// increment the counter variable with the increment in the ith variable //Jaise hi i ki value badhegi count ki value bhi 
     }
-    return count; //Return count because the value is present in count variable so we are returning the value of count...-2 because remove the new line character 
+    return count-2; //Return count because the value is present in count variable so we are returning the value of count...-2 because remove the new line character 
 }
 //puts() automatically move the cursor to the next line....we don't need to put the \n when using puts()....
