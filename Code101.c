48051c5288c6b88eaad931c13c9848f60f15a785
M_O_H_I_T3456 <61095721+mohit7386@users.noreply.github.com>
2024-02-26 17:52:39 +0530

    New Changes Added

--- Code101.c
+++ Code101.c
@@ -1,7 +1,7 @@
 //WAP and print the detail of the Student using typedef keyword
 #include<stdio.h>
 #include<string.h>
-typedef struct ComputerEngineeringStudent { //here we are using the typedef keyword with the help of this keyword we can print the values with their alias names...benefit is we don't need to write the whole name if we use alias names..
+typedef struct ComputerEngineeringStudent { //here we are using the typedef keyword with the help of this keyword we can print the values with their alias names...benefit is we don't need to write the data type when we are creating the typedef..
     char name[100]; 
     int roll,collegecode;
     int IdNumber;
@@ -9,8 +9,10 @@ typedef struct ComputerEngineeringStudent { //here we are using the typedef keyw
  }coe; //here we are creating the alias names and it will always declare after the brackets..
  int main (){
     coe s1; //Here we only initialise the structure with their alias name..we don't need to write struct ComputerEngineeringStudent
+    //Benefit of writing the typedef is we don't need to write the data type we can only access anything with the alias names...
     s1.roll = 2134;
     s1.IdNumber=444;
+    printf("***-----------------------------------------------------------***\n");
     printf("Data of the Student using tydef keyword\n");
     strcpy(s1.name,"Mohit Pratap Singh");
     printf("Student Name: %s\n",s1.name);
