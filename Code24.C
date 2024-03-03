6bee709aa05f507f7baa3abf3d963f475a79c04f
M_O_H_I_T3456 <61095721+mohit7386@users.noreply.github.com>
2024-03-03 13:15:29 +0530

    Made some Changes

--- Code24.C
+++ Code24.C
@@ -4,8 +4,8 @@ int main(){
     int i=1;//first you need to initialise the variable i then only you will use it later otherwise it will give you the error 
     printf("Here are the Printed Numbers Using while loop\n");
     while(i<=5){//here we are checking the condition if it is true then it will print something 
-        printf("%d\n",i);
-        i++;
+        printf("%d\n",i); //Here we are printing the result
+        i++; //After printing the result we are incrementing the value...
     }
     return 0;
 }
