f0495c442ffd2953420a09057ef4c82ca95217bd
M_O_H_I_T3456 <61095721+mohit7386@users.noreply.github.com>
2024-03-05 19:20:04 +0530

    New Changes Added in the repository

--- Code57.C
+++ Code57.C
@@ -2,18 +2,18 @@
 #include<stdio.h>
 
 // Function declaration
-void increment(int x);
+void increment(int a);
 
 int main() {
-    int num = 5;
+    int num = 5; //setting the value of the num 
 
     printf("Before calling the function: %d\n", num);
 
     // Function call
     increment(num); // Using the value of 'num' in the function call
 
-    printf("After calling the function: %d\n", num);//num value will not affect because it uses the copy of the values not the original value so the original value will not affect by the change in the function definition 
-    printf("The value of num if we put the increment of 2 in it then it will only change and the value is: %d\n", num + 2);
+    printf("After calling the function: %d\n", num);//num value will not affect because it uses the copy of the values not the original value so the original value will not affect by the change in the function definition and here the value of x increases but the value of x in function decleration is not affecting the value in main..
+    printf("The value of num if we put the increment of 2 in it then it will only change the value in the main function: %d\n", num + 2); //The value will change only when we change in the main function...Otherwise the value is not changed
     return 0;
 }
 
@@ -22,6 +22,6 @@ void increment(int x) {
     // Increment the local variable x
     x++;
 
-    printf("Inside the function: %d\n", x);
+    printf("Inside the function the value of the x is: %d\n", x);
     // The value of 'x' is only changed inside the function, not affecting the 'num' in the main function
 }
